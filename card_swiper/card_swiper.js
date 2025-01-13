5e491034abac5d8eac52f6a4463f0407238eb0d1
Le Chen <LeChen11111@163.com>
2025-01-13 16:24:57 +0800

    card swiper: debug

--- card_swiper/card_swiper.js
+++ card_swiper/card_swiper.js
@@ -1,17 +1,19 @@
-const Swiper = function() {
-    this.imgArr = imgArr;
+const Swiper = function(obj) {
+    this.imgArr = obj.imgArr;
     this.retImgArr = [this.imgArr[this.imgArr.length - 2], this.imgArr[this.imgArr.length - 1], ...this.imgArr, this.imgArr[0], this.imgArr[1]];
-    this.aniTime = aniTime || 500;
-    this.intervalTime = intervalTime || 1000;
-    this.autoplay = autoplay || true;
+    this.aniTime = obj.aniTime || 500;
+    this.intervalTime = obj.intervalTime || 1000;
+    this.autoplay = obj.autoplay || true;
 
     this.container = document.querySelector('.swiper-container');
-    this.moveWidth = this.container.offsetWidth;
+    this.moveWidth = 640; // 1408
     this.nowIndex = 3;
     this.leftBtn;
     this.rightBtn;
     this.spotDom;
     this.mainDom;
+    this.gap = obj.gap || 0;
+    this.scale = obj.scale || 0.8;
 
     this.timer = null;
     this.prev = Date.now();
@@ -24,19 +26,19 @@ Swiper.prototype = {
         let li = '';
         for(let i = 0; i < this.retImgArr.length; i++) {
             if(i === 2 || i === 4) {
-                li += `<li class="swiper-item-small" style="left: ${i * this.moveWidth}px; width: ${this.moveWidth / 2}px; height: ${this.moveWidth / 2}px">
+                li += `<li class="swiper-item-small" style="left: ${i * this.moveWidth}px; width: ${this.moveWidth}px; height: 100%">
                 <a href="${this.retImgArr[i].url}">
                     <img src="${this.retImgArr[i].src}">
                 </a>
                 </li>`;
             }else if(i === 3) {
-                li += `<li class="swiper-item-big" style="left: ${i * this.moveWidth}px; width: ${this.moveWidth * 1.2}px; height: ${this.moveWidth * 1.2}px">
+                li += `<li class="swiper-item-big" style="left: ${i * this.moveWidth}px; width: ${this.moveWidth}px; height: 100%">
                 <a href="${this.retImgArr[i].url}">
                     <img src="${this.retImgArr[i].src}">
                 </a>
                 </li>`;
             }else {
-                li += `<li class="swiper-item" style="left: ${i * this.moveWidth}px; width: ${this.moveWidth}px">
+                li += `<li class="swiper-item" style="left: ${i * this.moveWidth}px; width: ${this.moveWidth}px; height: 100%">
                     <a href="${this.retImgArr[i].url}">
                         <img src="${this.retImgArr[i].src}">
                     </a>
@@ -59,31 +61,31 @@ Swiper.prototype = {
         this.eventBind();
     },
     initDom: function() {
-        const mainDom = document.createElement('ul');
-        mainDom.className = 'swiper-main';
-        mainDom.style.width = `${this.moveWidth * this.retImgArr.length}px`;
-        mainDom.style.left = `-${this.moveWidth * 2}px`;
-        this.container.appendChild(mainDom);
+        this.mainDom = document.createElement('ul');
+        this.mainDom.className = 'swiper-main';
+        this.mainDom.style.width = `${this.moveWidth * this.retImgArr.length}px`;
+        this.mainDom.style.left = `-${this.moveWidth * 2}px`;
+        this.container.appendChild(this.mainDom);
 
-        const spotDom = document.createElement('ul');
-        spotDom.className = 'swiper-spot';
-        this.container.appendChild(spotDom);
+        this.spotDom = document.createElement('ul');
+        this.spotDom.className = 'swiper-spot';
+        this.container.appendChild(this.spotDom);
 
-        const lastBtn = document.createElement('img');
-        lastBtn.className = 'swiper-last';
-        lastBtn.src = '../img/left.png';
-        this.container.appendChild(lastBtn);
+        this.lastBtn = document.createElement('img');
+        this.lastBtn.className = 'swiper-last';
+        this.lastBtn.src = '../img/left.png';
+        this.container.appendChild(this.lastBtn);
 
-        const nextBtn = document.createElement('img');
-        nextBtn.className = 'swiper-next';
-        nextBtn.src = '../img/right.png';
-        this.container.appendChild(nextBtn);
+        this.nextBtn = document.createElement('img');
+        this.nextBtn.className = 'swiper-next';
+        this.nextBtn.src = '../img/right.png';
+        this.container.appendChild(this.nextBtn);
     },
     nextSlider: function() {
         let that = this;
         that.nowIndex++;
         that.mainDom.style.transition = `left ${that.aniTime}ms`;
-        that.mianDom.style.left = `$-{that.nowIndex * that.moveWidth}px`;
+        that.mainDom.style.left = `$-{that.nowIndex * that.moveWidth}px`;
 
         if(that.nowIndex === that.retImgArr.length - 2) {
             that.nowIndex = 1;
@@ -98,6 +100,11 @@ Swiper.prototype = {
         }
     },
     prevSlider:function() {
+        if(this.imgArr.length === 1) {
+            return;
+        }else if(this.imgArr.length === 2) {
+            this.nowIndex = this.nowIndex ? 0 : 2;
+        }
         let that = this;
         that.nowIndex--;
         that.mainDom.style.transition = `right ${that.aniTime}ms`;
@@ -119,8 +126,8 @@ Swiper.prototype = {
         this.mainDom.children[this.nowIndex].style.width = `${this.moveWidth * 1.2}px`;
         this.mainDom.children[this.nowIndex].style.height = `${this.moveWidth * 1.2}px`;
         
-        this.mianDom.children[this.nowIndex - 1].style.width = `${this.moveWidth * 0.5}px`;
-        this.mianDom.children[this.nowIndex - 1].style.height = `${this.moveWidth * 0.5}px`;
+        this.mainDom.children[this.nowIndex - 1].style.width = `${this.moveWidth * 0.5}px`;
+        this.mainDom.children[this.nowIndex - 1].style.height = `${this.moveWidth * 0.5}px`;
 
         this.mainDom.children[this.nowIndex + 1].style.width = `${this.moveWidth * 0.5}px`;
         this.mainDom.children[this.nowIndex + 1].style.height = `${this.moveWidth * 0.5}px`;
